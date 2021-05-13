2e160ee0ea5e9a1b0454e7de2f6fbf98caa688fe
ipous <pousig@g.crookcountyschools.org>
2021-05-13 08:23:21 -0700

    Animal farm cleanup

---
+++ script/app.js
@@ -0,0 +1,25 @@
+const form = document.getElementsByTagName('FORM') [0];
+
+form.addEventListener('submit', makeNewArticle);
+
+
+
+function makeNewArticle(event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const journal = event.target.content.value;
+
+    const article = document.createElement('article');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+
+
+    h2.textContent = title;
+    p.textContent = journal;
+
+    article.appendChild(h2);
+    article.appendChild(p);
+
+    const main = document.querySelector('main')
+    main.appendChild(article);
+}
